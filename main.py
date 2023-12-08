57d4a1da53cd1768871723bb000a8fc352a9ec70
therealchrisbrown <ch.braun1@gmx.de>
2023-12-08 16:33:07 +0100

    main function and init main

--- main.py
+++ main.py
@@ -106,3 +106,53 @@ def on_click_callback():
     st.session_state.history.append(
         Message("AI", llm_response)
     )
+
+def main():
+
+    initialize_session_state()
+    chat_placeholder = st.container()
+    prompt_placeholder = st.form("chat-form")
+
+    with chat_placeholder:
+        for chat in st.session_state.history:
+            if type(chat.Message) is dict:
+                msg = chat.Message['answer']
+            else:
+                msg = chat.Message 
+            div = f"""
+            <div class = "chatRow 
+            {'' if chat.origin == 'AI' else 'rowReverse'}">
+                <img class="chatIcon" src = "app/static/{'elsa.png' if chat.origin == 'AI' else 'admin.png'}" width=32 height=32>
+                <div class = "chatBubble {'adminBubble' if chat.origin == 'AI' else 'humanBubble'}">&#8203; {msg}</div>
+            </div>"""
+            st.markdown(div, unsafe_allow_html=True)
+
+    with st.form(key="chat_form"):
+        cols = st.columns((6, 1))
+        # Display the initial message if it hasn't been sent yet
+        if not st.session_state.initial_message_sent:
+            cols[0].text_input(
+                "Chat",
+                placeholder="Hello, how can I assist you?",
+                label_visibility="collapsed",
+                key="customer_prompt",
+            )  
+        else:
+            cols[0].text_input(
+                "Chat",
+                value=st.session_state.input_value,
+                label_visibility="collapsed",
+                key="customer_prompt",
+            )
+
+        cols[1].form_submit_button(
+            "Ask",
+            type="secondary",
+            on_click=on_click_callback,
+        )
+
+    # Update the session state variable when the input field changes
+    st.session_state.input_value = cols[0].text_input
+
+if __name__ == "__main__":
+    main()
